59f39f1a6d0364263fb5fec3a3e21cbf97153e77
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-09-28 12:25:09 +0600

    Setup tailwind css with Create React App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p className="bg-indigo-400 text-3xl">This is my text</p>
+      <p className="bg-indigo-400 text-3xl hover:text-yellow-500">This is my text</p>
+      <p className="text-primary">hello text</p>
+      <p className="text-custom">custom style</p>
     </div>
   );
 }
